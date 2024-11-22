5e4af769ba9f888042e005389bfd4ca5a2dbc2cc
MohamedAttia3104 <attia3104@gmail.com>
2024-11-22 20:28:20 +0200

    setUp firebase app distribution

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -7,14 +7,19 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/di/dependency_injection.dart';
 import 'core/routing/app_router.dart';
 import 'home4u_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
   _setStatusBarColor();
   await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
-   Home4uApp(appRouter: AppRouter()),
+    Home4uApp(appRouter: AppRouter()),
   );
 }
 
