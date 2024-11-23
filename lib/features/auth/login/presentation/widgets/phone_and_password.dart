4267ce69c24bce9dceb43fa980c6cc57ee1ac9c5
MohamedAttia3104 <attia3104@gmail.com>
2024-11-23 18:03:10 +0200

    solve conflicts between us

--- lib/features/auth/login/presentation/widgets/phone_and_password.dart
+++ lib/features/auth/login/presentation/widgets/phone_and_password.dart
@@ -16,43 +16,6 @@ class PhoneAndPassword extends StatefulWidget {
 
 class _PhoneAndPasswordState extends State<PhoneAndPassword> {
   bool isObscureText = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AppTextFormField(
-          hintText: "phone number",
-          validator: (value) {
-            if (value.isEmpty ||
-                value == null ||
-                !AppRegex.isPhoneNumberValid(value)) {
-              return "Please enter a valid phone number";
-            }
-          },
-        ),
-        verticalSpace(16),
-        AppTextFormField(
-          hintText: "password",
-          validator: (value) {
-            if (value.isEmpty || value == null) {
-              return "Please Enter a valid Password";
-            }
-          },
-          prefixIcon: Icon(Icons.lock_open_outlined,size: 16.r,color: AppColors.blackColor,),
-          suffixIcon: GestureDetector(
-            onTap: () {
-              setState(() {
-                isObscureText = !isObscureText;
-              });
-            },
-            child: Icon(
-              isObscureText ? Icons.visibility_off : Icons.visibility,
-              color: AppColors.primaryColor,
-            ),
-          ),
-        )
-      ],
   final formKey = GlobalKey<FormState>();
   final phoneFocusNode = FocusNode();
   final passwordFocusNode = FocusNode();
