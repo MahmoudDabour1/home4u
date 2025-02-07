6edbcee45db51093641d811c4d5bec6d2979edf9
MohamedAttia3104 <attia3104@gmail.com>
2025-02-07 15:03:14 +0200

    - set up to production

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_localization/flutter_localization.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -9,20 +9,19 @@ import 'core/helpers/shared_pref_helper.dart';
 import 'core/helpers/shared_pref_keys.dart';
 import 'core/routing/app_router.dart';
 import 'core/utils/hive_set_up.dart';
-import 'features/profile/data/data_sources/profile_local_data_source.dart';
 import 'firebase_options.dart';
 import 'home4u_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setupGetIt();
-  _setStatusBarColor();
+  await setupGetIt();
   await ScreenUtil.ensureScreenSize();
   await Hive.initFlutter();
   await initHive();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FlutterLocalization.instance.ensureInitialized();
   final cachedLocale =
       await SharedPrefHelper.getString(SharedPrefKeys.selectedLocale);
   final initialLocale = cachedLocale.isNotEmpty ? cachedLocale : 'en';
@@ -31,14 +30,6 @@ void main() async {
     Home4uApp(
       appRouter: AppRouter(),
       initialLocale: initialLocale,
-    ),
-  );
-}
-
-void _setStatusBarColor() {
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.red,
     ),
   );
 }
