df5ff25d4d7141bf9340ce65718329acb1d38788
MohamedAttia3104 <attia3104@gmail.com>
2025-03-10 23:13:40 +0200

    - update business add product cubit

--- lib/features/exhibition/logic/business_add_product_cubit.dart
+++ lib/features/exhibition/logic/business_add_product_cubit.dart
@@ -5,25 +5,25 @@ import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:home4u/core/networking/dio_factory.dart';
+import 'package:home4u/features/exhibition/data/models/business_add_product_images_response.dart';
 import 'package:http_parser/http_parser.dart';
 import 'package:image_picker/image_picker.dart';
 
-import '../../../core/routing/router_observer.dart';
 import '../data/models/business_add_product_body.dart';
 import '../data/models/business_add_product_images_body.dart';
 import '../data/repository/business_add_product_repository.dart';
 import 'business_add_product_state.dart';
 
 class BusinessAddProductCubit extends Cubit<BusinessAddProductState> {
-  final BusinessAddProductRepository _businessAddProductRepository;
+  final BusinessAddProductRepository _repository;
 
-  BusinessAddProductCubit(this._businessAddProductRepository)
+  BusinessAddProductCubit(this._repository)
       : super(const BusinessAddProductState.initial());
 
-  /// General
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  /// Form Key
+  final formKey = GlobalKey<FormState>();
 
-  /// Basic Details Controllers
+  /// Controllers
   final productNameArController = TextEditingController(text: "عنوان المنتج");
   final productNameEnController = TextEditingController(text: "Product Title");
   final productDescriptionArController =
@@ -31,175 +31,143 @@ class BusinessAddProductCubit extends Cubit<BusinessAddProductState> {
   final productDescriptionEnController =
       TextEditingController(text: "Product Description");
   final productPriceController = TextEditingController(text: "20.0");
-  int? selectedBaseUnit;
-  int? selectedExhibitionBusinessType;
-
-  /// Materials and Specs Controllers
-  List<int>? selectedMaterials;
   final productLengthController = TextEditingController(text: "10.0");
   final productWidthController = TextEditingController(text: "10.0");
   final productHeightController = TextEditingController(text: "10.0");
-
-  /// Colors and Stock Controllers
   final productStockAmountController = TextEditingController();
-  int? selectedColor;
 
-  /// Stores the selected colors and stock amounts
+  /// Dropdown Selections
+  int? selectedBaseUnit;
+  int? selectedExhibitionBusinessType;
+  List<int>? selectedMaterials;
+  int? selectedColor;
   final List<Map<String, dynamic>> selectedColorsAndStock = [];
 
-  /// Method to update colors and stock
-  void updateSelectedColorsAndStock(List<Map<String, dynamic>> newList) {
-    selectedColorsAndStock.clear();
-    selectedColorsAndStock.addAll(newList);
-  }
-
   /// Images
-  List<File> images = [];
+  final List<File> images = [];
 
-  /// Select Image
-  void selectImage({required BuildContext context, ImageSource? source}) {
-    ImagePicker.platform
-        .getImageFromSource(
-      source: source!,
-    )
-        .then((value) {
-      if (value != null) {
-        final imageFile = File(value.path);
-        log("Selected image path: ${imageFile.path}"); // Debug log
-        images.add(imageFile);
-        emit(BusinessAddProductState.selectImageSuccess(images));
-        Navigator.pop(context);
-      } else {
-        emit(BusinessAddProductState.selectImageFailure(
-            "An error occurred while selecting image"));
-      }
-    });
+  void updateSelectedColorsAndStock(List<Map<String, dynamic>> newList) {
+    selectedColorsAndStock
+      ..clear()
+      ..addAll(newList);
   }
 
+  void selectImage(
+      {required BuildContext context, required ImageSource source}) async {
+    final pickedFile = await ImagePicker().pickImage(source: source);
+    if (pickedFile != null) {
+      final imageFile = File(pickedFile.path);
+      log("Selected image path: ${imageFile.path}");
+      images.add(imageFile);
+      emit(BusinessAddProductState.selectImageSuccess(images));
+    } else {
+      emit(
+          BusinessAddProductState.selectImageFailure("Image selection failed"));
+    }
+    Navigator.pop(context);
+  }
+
+  ///Step 1: Add Business Product
   Future<void> addProductAndImages() async {
-    /// Step 1: Add Product
     emit(const BusinessAddProductState.addBusinessProductLoading());
 
-    final List<Stock> stockList = selectedColorsAndStock.map((item) {
-      return Stock(
-        color: BaseUnit(id: item["colorId"]),
-        amount: item["stock"],
-      );
-    }).toList();
+    final stockList = selectedColorsAndStock
+        .map((item) => Stock(
+              color: BaseUnit(id: item["colorId"]),
+              amount: item["stock"],
+            ))
+        .toList();
 
-    logger.t("Selected Materials: $selectedMaterials");
-
-    final addProductResult =
-        await _businessAddProductRepository.addBusinessProduct(
-      BusinessAddProductBody(
-        nameAr: productNameArController.text,
-        nameEn: productNameEnController.text,
-        descriptionAr: productDescriptionArController.text,
-        descriptionEn: productDescriptionEnController.text,
-        businessType: BaseUnit(id: selectedExhibitionBusinessType!),
-        price: double.parse(productPriceController.text),
-        length: double.parse(productLengthController.text),
-        width: double.parse(productWidthController.text),
-        height: double.parse(productHeightController.text),
-        baseUnit: BaseUnit(id: selectedBaseUnit!),
-        materials:
-            selectedMaterials?.map((e) => BaseUnit(id: e)).toList() ?? [],
-        stocks: stockList,
-        imagePaths: [],
-      ),
+    final productBody = BusinessAddProductBody(
+      nameAr: productNameArController.text,
+      nameEn: productNameEnController.text,
+      descriptionAr: productDescriptionArController.text,
+      descriptionEn: productDescriptionEnController.text,
+      businessType: BaseUnit(id: selectedExhibitionBusinessType!),
+      price: double.parse(productPriceController.text),
+      length: double.parse(productLengthController.text),
+      width: double.parse(productWidthController.text),
+      height: double.parse(productHeightController.text),
+      baseUnit: BaseUnit(id: selectedBaseUnit!),
+      materials: selectedMaterials?.map((e) => BaseUnit(id: e)).toList() ?? [],
+      stocks: stockList,
+      imagePaths: [],
     );
 
+    final addProductResult = await _repository.addBusinessProduct(productBody);
     addProductResult.when(
       success: (productResponse) async {
         emit(
             BusinessAddProductState.addBusinessProductSuccess(productResponse));
-
-        /// Step 2: Add Product Images
-        emit(const BusinessAddProductState.addBusinessProductImageLoading());
-
-        // Prepare a list of BusinessAddProductImagesBody
-        final List<BusinessAddProductImagesBody> imageBodies =
-            images.map((imageFile) {
-          return BusinessAddProductImagesBody(
-            productId: productResponse.data.id,
-            imagePath: null, // Use the image file path if needed
-          );
-        }).toList();
-
-        // Send the list of images
-        final addImageResult = await _businessAddProductRepository
-            .addBusinessProductImage(imageBodies);
-
-        addImageResult.when(
-          success: (imageResponse) async {
-            logger
-                .e('Image uploaded successfully: ${imageResponse.toString()}');
-            emit(BusinessAddProductState.addBusinessProductImageSuccess(
-                imageResponse));
-
-            ///ToDo
-            /// Step 3: Upload Images
-            emit(const BusinessAddProductState.uploadBusinessImageLoading());
-
-            DioFactory.setContentTypeForMultipart();
-
-            final imageFiles = await Future.wait(images.map((image) async {
-              return await MultipartFile.fromFile(
-                image.path,
-                filename: image.path.split('/').last,
-                contentType: MediaType('image', 'jpeg'),
-              );
-            }));
-
-            for (var i = 0; i < imageFiles.length; i++) {
-              final imageFile = imageFiles[i];
-              final imageId = imageResponse.data[i].id;
-
-              final uploadResult =
-                  await _businessAddProductRepository.uploadBusinessImage(
-                "BUSINESS_PRODUCTS",
-                imageId,
-                FormData.fromMap({
-                  'file': imageFile,
-                }),
-              );
-
-              uploadResult.when(
-                success: (uploadResponse) {
-                  logger.w(
-                      'Image uploaded successfully: ${uploadResponse.toString()}'); // Debug log
-                  if (uploadResponse.success) {
-                    emit(BusinessAddProductState.uploadBusinessImageSuccess());
-                  } else {
-                    emit(BusinessAddProductState.uploadBusinessImageFailure(
-                        uploadResponse.data.toString()));
-                  }
-                },
-                failure: (error) {
-                  logger
-                      .d('Image upload failed: ${error.message}'); // Debug log
-
-                  emit(BusinessAddProductState.uploadBusinessImageFailure(
-                      error.message.toString()));
-                },
-              );
-            }
-          },
-          failure: (error) {
-            logger.e('Image upload failed: ${error.message}');
-            emit(BusinessAddProductState.addBusinessProductImageFailure(
-                error.message.toString()));
-          },
-        );
+        await _addBusinessProductImages(productResponse.data.id);
       },
-      failure: (error) {
-        emit(BusinessAddProductState.addBusinessProductFailure(
-            error.message.toString()));
-      },
+      failure: (error) => emit(
+          BusinessAddProductState.addBusinessProductFailure(
+              error.message.toString())),
     );
   }
 
-  ///Preview
+  ///Step 2: Add Business Product Images
+  Future<void> _addBusinessProductImages(int productId) async {
+    emit(const BusinessAddProductState.addBusinessProductImageLoading());
+
+    final imageBodies = images
+        .map((image) => BusinessAddProductImagesBody(
+              productId: productId,
+              imagePath: null,
+            ))
+        .toList();
+
+    final addImageResult =
+        await _repository.addBusinessProductImage(imageBodies);
+    addImageResult.when(
+      success: (imageResponse) async {
+        emit(BusinessAddProductState.addBusinessProductImageSuccess(
+            imageResponse));
+        await _uploadBusinessImages(imageResponse);
+      },
+      failure: (error) => emit(
+          BusinessAddProductState.addBusinessProductImageFailure(
+              error.message.toString())),
+    );
+  }
+
+  ///Step 3: Upload Business Images
+  Future<void> _uploadBusinessImages(
+      BusinessAddProductImagesResponse imageResponse) async {
+    emit(const BusinessAddProductState.uploadBusinessImageLoading());
+    DioFactory.setContentTypeForMultipart();
+
+    final imageFiles =
+        await Future.wait(images.map((image) => MultipartFile.fromFile(
+              image.path,
+              filename: image.path.split('/').last,
+              contentType: MediaType('image', 'jpeg'),
+            )));
+
+    for (var i = 0; i < imageFiles.length; i++) {
+      final uploadResult = await _repository.uploadBusinessImage(
+        "BUSINESS_PRODUCTS",
+        imageResponse.data[i].id,
+        FormData.fromMap({'file': imageFiles[i]}),
+      );
+      uploadResult.when(
+        success: (uploadResponse) {
+          if (uploadResponse.success) {
+            emit(BusinessAddProductState.uploadBusinessImageSuccess());
+          } else {
+            emit(BusinessAddProductState.uploadBusinessImageFailure(
+                uploadResponse.data.toString()));
+          }
+        },
+        failure: (error) => emit(
+            BusinessAddProductState.uploadBusinessImageFailure(
+                error.message.toString())),
+      );
+    }
+  }
+
+  ///Product Preview
   Map<String, dynamic> getPreviewData() {
     return {
       'productName': productNameEnController.text,
