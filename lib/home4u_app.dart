ec54a5724e72e4be44d25458b14d4ccdf1a65a11
DESKTOP-1PPVHEP\Mohamed Attiya <attia3104@gmail.com>
2025-04-19 00:37:15 +0200

    - track files

--- lib/home4u_app.dart
+++ lib/home4u_app.dart
@@ -153,8 +153,9 @@ class _Home4uAppState extends State<Home4uApp> {
                     onGenerateRoute: widget.appRouter.generateRoute,
                     debugShowCheckedModeBanner: false,
                     navigatorObservers: [NavigatorObserver(), _routeObserver],
-                    initialRoute: Routes.loginScreen,
-                    // snapshot.data!,
+                    initialRoute:
+                        // Routes.loginScreen,
+                        snapshot.data!,
                   ),
                 );
               },
