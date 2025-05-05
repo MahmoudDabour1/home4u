c836d63e239233a76891c0fb3fa3a1f4aa8bbb61
DESKTOP-1PPVHEP\Mohamed Attiya <attia3104@gmail.com>
2025-05-06 01:39:21 +0300

    - track generated files

--- lib/features/cart/logic/cart_cubit.dart
+++ lib/features/cart/logic/cart_cubit.dart
@@ -1,15 +1,34 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:home4u/features/cart/data/models/shop_now_search_body.dart';
+import 'package:logger/logger.dart';
 
 import '../../../core/routing/router_observer.dart';
+import '../../products/data/models/business_config_model.dart';
 import '../data/models/shop_now_response_model.dart';
 import '../data/repository/cart_repository.dart';
 import 'cart_state.dart';
 
 class CartCubit extends Cubit<CartState> {
   final CartRepository cartRepository;
+  BusinessConfigModel? businessConfigModel;
 
   CartCubit(this.cartRepository) : super(const CartState.initial());
+
+
+  ///resolve filter
+  List<String>? selectedBusinessTypeNames = [];
+  List<String>? selectedMaterialNames = [];
+  List<String>? selectedColorNames = [];
+  double? minPrice;
+  double? maxPrice;
+  List<int?> colorsIds = [];
+  List<int?> businessTypeIds = [];
+  List<int?> materialIds = [];
+  bool? isAvailable;
+  final searchController = TextEditingController();
+  int? selectedBusinessType;
+  int? selectedBusinessTypeCategory;
 
   ///pagination
   int _page = 0;
@@ -82,5 +101,24 @@ class CartCubit extends Cubit<CartState> {
       }
     });
     isFetching = false;
+  }
+
+  Future<void> resetFilter() async {
+    selectedBusinessTypeNames = [];
+    selectedMaterialNames = [];
+    selectedColorNames = [];
+    minPrice = null;
+    maxPrice = null;
+    colorsIds = [];
+    businessTypeIds = [];
+    materialIds = [];
+    isAvailable = null;
+    searchController.clear();
+  }
+
+  Future<void> resetPagination() async {
+    _page = 0;
+    hasReachedMax = false;
+    products = [];
   }
 }
